3d44039ecbb0ef24cf0cbb42bb0cf8f5486b42c3
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-01-26 22:18:26 +0000

    add toping on Casting in Kotlin

--- src/KotlinTut.kt
+++ src/KotlinTut.kt
@@ -60,4 +60,11 @@ fun main(args: Array<String>) {
 //    Char
     val letterGrade: Char = 'A'
     println("A is a Char: ${letterGrade is Char}")
+
+    /*CASTING*/
+    println("3.14 to Int: " + (3.14.toInt()))
+    println("A to Int: " + ('A'.toInt()))
+    println("65 to Char: "+ 65.toChar())
+
+    /*STRINGS*/
 }
