2a6919b7899dcb206a4a97b3bc9063c9153a2a88
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-01-29 14:12:54 +0000

    add Array Collection topic

--- src/KotlinTut.kt
+++ src/KotlinTut.kt
@@ -122,4 +122,18 @@ fun main(args: Array<String>) {
     val partArray = myArray.copyOfRange(0, 1)
     println("First: ${myArray.first()}")
     println("Artemas index: ${myArray.indexOf("Artemas")}")
+
+    println()
+    println("/*COLLECTIONS*/")
+    println("==============")
+
+//    Sqaure Array
+    val sqArray = Array(5) { x -> x * x}
+    println(sqArray[2]) // Because 4 = 2 * 2
+
+//    Typed Array
+    val arr2: Array<Int> = arrayOf(1, 2, 3)
+//    val arr2: Array<Int> = arrayOf(1, 2, 3, "Hello") <-- NOT VALID
+
+//    Ranges
 }
