2bc7b0b4c55e66fed3b4d100f80fbd4a42e76768
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-01 01:16:40 +0000

    add Conditionals topic

--- src/KotlinTut.kt
+++ src/KotlinTut.kt
@@ -155,4 +155,35 @@ fun main(args: Array<String>) {
 
     val range3 = oneToTen.step(3)
     for (x in range3) println("RANGE 3 : $x")
+
+    println()
+    println("/*CONDITIONALS*/")
+    println("==============")
+
+    val myAge = 8
+
+    if (myAge < 5) {
+        println("Go to Preschool")
+    } else if (myAge == 5) {
+        println("Go to Kindergarten")
+    } else if ((myAge > 5) && (myAge <= 17)) {
+        val grade = myAge - 5
+        println("Go to Grade $grade")
+    } else {
+        println("Go to College")
+    }
+
+//    When
+    when (myAge){
+        0,1,2,3,4 -> println("Go to Preschool")
+
+        5 -> println("Go to Kindergarten")
+
+        in 6..17 -> {
+            val grade = myAge - 5
+            println("Go to Grade $grade")
+        }
+
+        else -> println("Go to College")
+    }
 }
