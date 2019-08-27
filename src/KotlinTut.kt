02bcbe9270da80f5542cd2f7a9887157fb436a48
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-08-27 08:54:24 +0100

    Add filter

--- src/KotlinTut.kt
+++ src/KotlinTut.kt
@@ -3,5 +3,6 @@ fun main(args: Array<String>) {
     val numberStringList = listOf("1", "2", "3", "10", "20")
     numberStringList
             .map { Integer.valueOf(it)}
+            .filter { it < 10 }
             .forEach { print(it) }
 }
