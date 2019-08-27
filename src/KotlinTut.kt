c8d0b44c015340a6bc92bfa28b6ed086e14ce775
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-08-27 18:44:22 +0100

    Add Student objects

--- src/KotlinTut.kt
+++ src/KotlinTut.kt
@@ -1,8 +1,36 @@
-fun main(args: Array<String>) {
+import kotlin.streams.toList
 
+fun main(args: Array<String>) {
+    println(getNumbersLessThan10())
+    println(getLetterUpperCasedAndOrdered())
+}
+
+fun getNumbersLessThan10(): List<Int> {
     val numberStringList = listOf("1", "2", "3", "10", "20")
-    numberStringList
+    return numberStringList
             .map { Integer.valueOf(it)}
             .filter { it < 10 }
-            .forEach { print(it) }
+}
+
+fun getLetterUpperCasedAndOrdered(): List<String> {
+    val letters = listOf("z", "c", "h", "a", "j", "f", "i", "b")
+    return letters
+            .stream()
+            .map { it.toUpperCase() }
+            .sorted()
+            .sequential()
+            .toList()
+}
+
+data class Student(
+        val name: String,
+        val surname: String,
+        val age: Int,
+        val subjects: List<String>
+)
+
+fun generatedStudents() {
+    val artemas = Student("Artemas", "Muzanenhamo", 29, listOf("English", "Computer Science", "Mathematics"))
+    val thomas = Student("Thomas", "Jenkins", 32, listOf("Geography", "English", "Law"))
+    val kirsty = Student("Kirsty", "Smith", 30, listOf("Bio-Chemistry", "Mathematics", "Information Systems"))
 }
