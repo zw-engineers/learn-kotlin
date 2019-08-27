9cd17d7cce80cb8985899fe05ca28dbda09f335b
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-08-27 08:52:52 +0100

    Add small Kotlin demo to use forEach()

--- src/KotlinTut.kt
+++ src/KotlinTut.kt
@@ -1,3 +1,7 @@
 fun main(args: Array<String>) {
 
+    val numberStringList = listOf("1", "2", "3", "10", "20")
+    numberStringList
+            .map { Integer.valueOf(it)}
+            .forEach { print(it) }
 }
