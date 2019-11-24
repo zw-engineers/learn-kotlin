e5d2a69256c14de7ea8e7f182b9b24aba4bac5bf
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-11-24 12:20:45 +0000

    inline orEmpty() as it applies to the list as opposed to the element

--- src/main/kotlin/learn/kotlin/functional/CapitaliseNames.kt
+++ src/main/kotlin/learn/kotlin/functional/CapitaliseNames.kt
@@ -2,8 +2,7 @@ package learn.kotlin.functional
 
 class CapitaliseNames {
     fun capitalise(names: List<String?>?): List<String> {
-        return names
-                .orEmpty()
+        return names.orEmpty()
                 .map(String?::orEmpty)
                 .filter(String::isNotEmpty)
                 .map(String::toUpperCase)
